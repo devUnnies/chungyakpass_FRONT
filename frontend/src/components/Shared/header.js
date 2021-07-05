b929b64eb7a36fede186cad4a6a0409cbbea0249
jejecrunch <gkswlgp456@gmail.com>
2021-07-06 01:56:12 +0900

    header login 영역, logo 영역 완료

---
+++ frontend/src/components/Shared/header.js
@@ -0,0 +1,61 @@
+import { React } from "react";
+import "./header.css";
+import logo from "../../assets/logo.svg";
+import data from "../../category.json";
+
+function Login() {
+  return (
+    <div className="login">
+      <div className="login-container">
+        <div className="login-subcontainer">
+          <div className="login-buttonArea">
+            <div className="login-loginButton">로그인</div>
+            <div className="login-signUpButton">회원가입</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <div className="logo-container">
+        <div className="logo-image">
+          <img src={logo} alt="logo"></img>
+        </div>
+        <div className="logo-name">
+          청약<i>pass</i>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Nav() {
+  return (
+    <div className="nav">
+      <div className="nav-container">
+        {/** 여러 개의 카테고리를 관리 */}
+        <ul className="nav-items">
+          {data.category.map((content, i) => {
+            return <li className="nav-item">{content.name}</li>;
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+const header = () => {
+  return (
+    <div>
+      <Login></Login>
+      <Logo></Logo>
+      <Nav></Nav>
+    </div>
+  );
+};
+
+export default header;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import PersonalInfo from './components/CommonInput/PersonalInfo';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import Header from "./components/Shared/header.js";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <PersonalInfo />
+    <Header />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
