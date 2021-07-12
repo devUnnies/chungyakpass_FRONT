39b3b4a39e2eec482fb220c1a0cce42897467a7b
한지혜 <de1et_@naver.com>
2021-07-13 01:50:19 +0900

    footer 완성, 폴더 정리, redux 준비, signup 화면과 login 화면 준비

---
+++ frontend/src/pages/Signup/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Signup.css";
+
+const Signup = () => {
+  return (
+    <div className="Signup">
+      <h1>회원가입</h1>
+    </div>
+  );
+};
+
+export default Signup;
