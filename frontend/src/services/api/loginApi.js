d90117aee03b6e58776ac1d6dcc92fc52c9ea00e
한지혜 <de1et_@naver.com>
2021-08-25 23:38:00 +0900

    210825 회원가입 로그인 api 주소 변경

--- frontend/src/services/api/loginApi.js
+++ frontend/src/services/api/loginApi.js
@@ -2,15 +2,17 @@ import axios from 'axios';
 
 //로그인 및 회원가입 API
 export const logIn = (info) =>
-    axios.post('authenticate', info, {
+    axios.post('account/authenticate', info, {
         headers: {
             'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
         },
     });
 export const logOut = () => axios.post('logout', {});
 export const signUp = (info) =>
-    axios.post('signup', info, {
+    axios.post('account/signup', info, {
         headers: {
             'Access-Control-Allow-Origin': '*',
+            'Content-Type': 'application/json',
         },
     });
