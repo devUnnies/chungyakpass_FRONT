12e89a5d293607370fdc94a87f2f24deeb74797a
한지혜 <de1et_@naver.com>
2021-10-08 01:56:43 +0900

    211007 로그아웃 고침

--- frontend/src/store/reducers/index.js
+++ frontend/src/store/reducers/index.js
@@ -1,18 +1,28 @@
 import { combineReducers } from 'redux';
 import auth from './authReducer';
 import token from './tokenReducer';
+import commonInfo from './commonInfoReducer';
 import generalMinyeong from './generalMinyeongReducer';
 import generalMinyeongAptNum from './generalMinyeongAptNumReducer';
 import multiChildMinyeong from './multiChildMinyeongAptNumReducer';
 import multiChildMinyeongAptNum from './multiChildMinyeongAptNumReducer';
 
-const rootReducer = combineReducers({
+const appReducer = combineReducers({
     auth,
     token,
+    commonInfo,
     generalMinyeong,
     generalMinyeongAptNum,
     multiChildMinyeong,
     multiChildMinyeongAptNum,
 });
+
+const rootReducer = (state, action) => {
+    if (action.type === 'LOGOUT') {
+        return appReducer(undefined, action);
+    }
+
+    return appReducer(state, action);
+};
 
 export default rootReducer;
