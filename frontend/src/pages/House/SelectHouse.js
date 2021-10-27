cceaf2d29f770abe1b06bf876eef55416e0f5837
한지혜 <de1et_@naver.com>
2021-10-28 04:15:45 +0900

    211027 세대 선택하는 화면 완료

---
+++ frontend/src/pages/House/SelectHouse.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import './House.css';
+import MainButton from '../../components/Button/MainButton';
+import { useHistory } from 'react-router';
+
+const SelectHouse = () => {
+    const [house, setHouse] = useState();
+
+    const history = useHistory();
+
+    const onChange = (e) => {
+        setHouse(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // 해당 하우스 값 세대 등록 페이지로 전달하기
+        history.push('/addHouse', { houseState: house });
+    };
+
+    return (
+        <div className="selectHouseContainer">
+            <div className="selectHouseHeaderContainer">
+                <div className="heightBar"></div>
+                <span className="listTitle">세대 선택</span>
+            </div>
+
+            <form
+                className="selectHouseForm"
+                onSubmit={handleSubmit}
+                name="selectHouse"
+            >
+                <input
+                    className="selectHouseFormInput"
+                    type="radio"
+                    name="house"
+                    onChange={onChange}
+                    value="my"
+                    checked={house === 'my' ? true : false}
+                    required
+                />
+
+                <span> 본인 세대</span>
+                <br />
+                <br />
+                <input
+                    className="selectHouseFormInput"
+                    type="radio"
+                    name="house"
+                    onChange={onChange}
+                    value="spouse"
+                    checked={house === 'spouse' ? true : false}
+                    required
+                />
+
+                <span> 배우자 분리 세대</span>
+
+                <div className="saveButtonContainer">
+                    <MainButton
+                        type="submit"
+                        className="save"
+                        width="80"
+                        height="30"
+                    >
+                        다음
+                    </MainButton>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default SelectHouse;
