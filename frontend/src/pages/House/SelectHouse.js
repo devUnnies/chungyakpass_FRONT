ebd2eeafc38169ea188ed394c1a4660ff4dfd556
한지혜 <de1et_@naver.com>
2021-10-31 06:02:57 +0900

    211030 세대 선택 화면 완료

--- frontend/src/pages/House/SelectHouse.js
+++ frontend/src/pages/House/SelectHouse.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './House.css';
 import MainButton from '../../components/Button/MainButton';
+import NextButton from '../../components/Button/NextButton';
 import { useHistory } from 'react-router';
 
 const SelectHouse = () => {
@@ -13,7 +14,7 @@ const SelectHouse = () => {
     };
 
     const handleSubmit = (e) => {
-        e.preventDefault();
+        // e.preventDefault();
 
         // 해당 하우스 값 세대 등록 페이지로 전달하기
         history.push('/addHouse', { houseState: house });
@@ -26,47 +27,71 @@ const SelectHouse = () => {
                 <span className="listTitle">세대 선택</span>
             </div>
 
-            <form
+            <div className="selectHouseFormContainer">
+                <table className="selectHouseFormTable">
+                    <tbody>
+                        <tr>
+                            <td className="selectHouseFormTableTrTd">
+                                <input
+                                    className="selectHouseFormInput"
+                                    type="radio"
+                                    name="house"
+                                    onChange={onChange}
+                                    value="my"
+                                    checked={house === 'my' ? true : false}
+                                    required
+                                />
+
+                                <span> 본인 세대</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="selectHouseFormTableTrTd">
+                                <input
+                                    className="selectHouseFormInput"
+                                    type="radio"
+                                    name="house"
+                                    onChange={onChange}
+                                    value="spouse"
+                                    checked={house === 'spouse' ? true : false}
+                                    required
+                                />
+
+                                <span> 배우자 분리 세대</span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="selectHouseFormSaveTd">
+                                <div className="saveButtonContainer">
+                                    <NextButton
+                                        width={50}
+                                        height={50}
+                                        className="selectHouseButton"
+                                        type="selectHouse"
+                                        fontSize={150}
+                                        onClick={() => handleSubmit()}
+                                    />
+                                    {/* <MainButton
+                                        type="submit"
+                                        className="save"
+                                        width="80"
+                                        height="30"
+                                        onClick={handleSubmit}
+                                    >
+                                        다음
+                                    </MainButton> */}
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            {/* <form
                 className="selectHouseForm"
                 onSubmit={handleSubmit}
                 name="selectHouse"
-            >
-                <input
-                    className="selectHouseFormInput"
-                    type="radio"
-                    name="house"
-                    onChange={onChange}
-                    value="my"
-                    checked={house === 'my' ? true : false}
-                    required
-                />
-
-                <span> 본인 세대</span>
-                <br />
-                <br />
-                <input
-                    className="selectHouseFormInput"
-                    type="radio"
-                    name="house"
-                    onChange={onChange}
-                    value="spouse"
-                    checked={house === 'spouse' ? true : false}
-                    required
-                />
-
-                <span> 배우자 분리 세대</span>
-
-                <div className="saveButtonContainer">
-                    <MainButton
-                        type="submit"
-                        className="save"
-                        width="80"
-                        height="30"
-                    >
-                        다음
-                    </MainButton>
-                </div>
-            </form>
+            ></form> */}
         </div>
     );
 };
