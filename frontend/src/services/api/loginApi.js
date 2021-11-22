ae06e95f88fe67791347edbd8343c58b11a38163
한지혜 <de1et_@naver.com>
2021-11-23 06:37:14 +0900

    211122 #3 id와 pw 불일치, 가입하지 않은 회원, 이메일 중복 에러메시지 추가

--- frontend/src/services/api/loginApi.js
+++ frontend/src/services/api/loginApi.js
@@ -1,5 +1,12 @@
 import { post } from './instance';
 
 //로그인 및 회원가입 API
-export const logIn = (info) => post('account/authenticate', info);
-export const signUp = (info) => post('account/signup', info);
+export const logIn = (info) =>
+    post('account/authenticate', info).catch((error) => {
+        console.log(error);
+        alert('아이디나 비밀번호를 잘못 입력하셨습니다 !');
+    });
+export const signUp = (info) =>
+    post('account/signup', info).catch((error) => {
+        console.log(error);
+    });
