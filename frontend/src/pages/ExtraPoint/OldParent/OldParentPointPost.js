096cdb4eaf36b32ccd16e1ea74b566f827887618
신수연 <shinsuyeon0424@gmail.com>
2021-11-26 02:19:00 +0900

    211126 일반민영, 노부모 민영 가배점계산기 완성.

--- frontend/src/pages/ExtraPoint/OldParent/OldParentPointPost.js
+++ frontend/src/pages/ExtraPoint/OldParent/OldParentPointPost.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import Input from '../../../components/Input/Input';
-import useInputState from '../../../components/Input/useInputState';
-import { CalculatorOutlined, CheckOutlined } from '@ant-design/icons';
+import { CalculatorOutlined } from '@ant-design/icons';
 import { postOldParentPoint } from '../../../store/actions/pointSpecialOldParentAction';
 import { useHistory } from 'react-router-dom';
+import ExtraPointTable from '../ExtraPointTable';
 import '../../ExtraPoint/ExtraPoint.css';
 
 function OldParentPointPost(props) {
@@ -19,78 +18,23 @@ function OldParentPointPost(props) {
         }
     };
 
-    const [houseMemberId, setHouseMemberId, handleChangeHouseMemberId] =
-        useInputState('');
-
-    const [parentsDeathYn, setParentsDeathYn, handleChangeParentsDeathYn] =
-        useInputState('');
-
-    const [divorceYn, setDivorceYn, handleChangeDivorceYn] = useInputState('');
-
-    const [
-        sameResidentRegistrationYn,
-        setSameResidentRegistrationYn,
-        handleChangeSameResidentRegistrationYn,
-    ] = useInputState('');
-
-    const [stayOverYn, setStayOverYn, handleChangeStayOverYn] =
-        useInputState('');
-
-    const [nowStayOverYn, setNowStayOverYn, handleChangeNowStayOverYn] =
-        useInputState('');
-
     const handleSubmit = (event) => {
         // 이전의 값을 가지고 와서 기본값으로 세팅
         event.preventDefault();
 
         // 연결해서 전체 저장소에 제대로 들어가는지 콘솔에서 확인하기
-        dispatch(
-            postOldParentPoint({
-                houseMemberId: houseMemberId,
-                parentsDeathYn: parentsDeathYn,
-                divorceYn: divorceYn,
-                sameResidentRegistrationYn: sameResidentRegistrationYn,
-                stayOverYn: stayOverYn,
-                nowStayOverYn: nowStayOverYn,
-            })
-        );
+        dispatch(postOldParentPoint({}));
     };
 
     const onClick = async () => {
-        if (
-            parentsDeathYn == '' &&
-            divorceYn == '' &&
-            sameResidentRegistrationYn == '' &&
-            stayOverYn == '' &&
-            nowStayOverYn == ''
-        ) {
-            alert('필수 입력 값 중 비어있는 값이 존재합니다.');
-        } else {
-            dispatch(
-                postOldParentPoint({
-                    houseMemberId: houseMemberId,
-                    parentsDeathYn: parentsDeathYn,
-                    divorceYn: divorceYn,
-                    sameResidentRegistrationYn: sameResidentRegistrationYn,
-                    stayOverYn: stayOverYn,
-                    nowStayOverYn: nowStayOverYn,
-                })
-            ); // api 연결 요청.
+        dispatch(postOldParentPoint({})); // api 연결 요청.
 
-            const data = oldParentPointStore?.postOldParentPoint?.data;
-            console.log(JSON.stringify(data));
-            history.push({
-                pathname: '/point/oldParent',
-                state: {
-                    houseMemberId,
-                    parentsDeathYn,
-                    divorceYn,
-                    sameResidentRegistrationYn,
-                    stayOverYn,
-                    nowStayOverYn,
-                },
-            });
-        }
+        const data = oldParentPointStore?.postOldParentPoint?.data;
+        console.log(JSON.stringify(data));
+        history.push({
+            pathname: '/point/oldParent',
+            state: {},
+        });
     };
 
     useEffect(() => {
@@ -112,236 +56,30 @@ function OldParentPointPost(props) {
                 </span>
             </div>
 
+            <ExtraPointTable />
+
             <div className="specialAptNumForm">
-                <div className="specialAptNumContainer">
-                    <form
-                        onSubmit={handleSubmit}
-                        onKeyPress={onKeyPress}
-                        className="specialAptNumForm"
-                    >
-                        <table border="1" className="pointTable">
-                            <tr className="paramSelect">
-                                <td className="pointTable_list">
-                                    <span className="qualificationBoxTitle">
-                                        부모 사망 여부
-                                    </span>
-                                </td>
-                                <td className="pointTable_select">
-                                    <input
-                                        className="paramSelectInput"
-                                        type="radio"
-                                        name="parentsDeathYn"
-                                        onChange={handleChangeParentsDeathYn}
-                                        value="y"
-                                        checked={
-                                            parentsDeathYn === 'y'
-                                                ? true
-                                                : false
-                                        }
-                                    />
-                                    <span className="selectInputText">
-                                        해당함
-                                    </span>
-                                    <input
-                                        className="paramSelectInput"
-                                        type="radio"
-                                        name="parentsDeathYn"
-                                        onChange={handleChangeParentsDeathYn}
-                                        value="n"
-                                        checked={
-                                            parentsDeathYn === 'n'
-                                                ? true
-                                                : false
-                                        }
-                                    />
-                                    <span className="selectInputText">
-                                        해당하지 않음
-                                    </span>
-                                </td>
-                            </tr>
-
-                            <br />
-                            <tr className="paramSelect">
-                                <td className="pointTable_list">
-                                    <span className="qualificationBoxTitle">
-                                        이혼 여부
-                                    </span>
-                                </td>
-                                <td className="pointTable_select">
-                                    <input
-                                        className="paramSelectInput"
-                                        type="radio"
-                                        name="divorceYn"
-                                        onChange={handleChangeDivorceYn}
-                                        value="y"
-                                        checked={
-                                            divorceYn === 'y' ? true : false
-                                        }
-                                    />
-                                    <span className="selectInputText">
-                                        해당함
-                                    </span>
-                                    <input
-                                        className="paramSelectInput"
-                                        type="radio"
-                                        name="divorceYn"
-                                        onChange={handleChangeDivorceYn}
-                                        value="n"
-                                        checked={
-                                            divorceYn === 'n' ? true : false
-                                        }
-                                    />
-                                    <span className="selectInputText">
-                                        해당하지 않음
-                                    </span>
-                                </td>
-                            </tr>
-
-                            <br />
-                            <tr className="paramSelect">
-                                <td className="pointTable_list">
-                                    <span className="qualificationBoxTitle">
-                                        동일 주소지 거주 여부
-                                    </span>
-                                </td>
-                                <td className="pointTable_select">
-                                    <input
-                                        className="paramSelectInput"
-                                        type="radio"
-                                        name="sameResidentRegistrationYn"
-                                        onChange={
-                                            handleChangeSameResidentRegistrationYn
-                                        }
-                                        value="y"
-                                        checked={
-                                            sameResidentRegistrationYn === 'y'
-                                                ? true
-                                                : false
-                                        }
-                                    />
-                                    <span className="selectInputText">
-                                        해당함
-                                    </span>
-                                    <input
-                                        className="paramSelectInput"
-                                        type="radio"
-                                        name="sameResidentRegistrationYn"
-                                        onChange={
-                                            handleChangeSameResidentRegistrationYn
-                                        }
-                                        value="n"
-                                        checked={
-                                            sameResidentRegistrationYn === 'n'
-                                                ? true
-                                                : false
-                                        }
-                                    />
-                                    <span className="selectInputText">
-                                        해당하지 않음
-                                    </span>
-                                </td>
-                            </tr>
-
-                            <br />
-
-                            <tr className="paramSelect">
-                                <td className="pointTable_list">
-                                    <span className="qualificationBoxTitle">
-                                        해외 혹은 요양시설 체류 여부
-                                    </span>
-                                </td>
-                                <td className="pointTable_select">
-                                    <input
-                                        className="paramSelectInput"
-                                        type="radio"
-                                        name="stayOverYn"
-                                        onChange={handleChangeStayOverYn}
-                                        value="y"
-                                        checked={
-                                            stayOverYn === 'y' ? true : false
-                                        }
-                                    />
-                                    <span className="selectInputText">
-                                        해당함
-                                    </span>
-                                    <input
-                                        className="paramSelectInput"
-                                        type="radio"
-                                        name="stayOverYn"
-                                        onChange={handleChangeStayOverYn}
-                                        value="n"
-                                        checked={
-                                            stayOverYn === 'n' ? true : false
-                                        }
-                                    />
-                                    <span className="selectInputText">
-                                        해당하지 않음
-                                    </span>
-                                </td>
-                            </tr>
-
-                            <br />
-
-                            <tr className="paramSelect">
-                                <td className="pointTable_list">
-                                    <span className="qualificationBoxTitle">
-                                        현재 해외 체류 여부
-                                    </span>
-                                </td>
-                                <td className="pointTable_select">
-                                    <input
-                                        className="paramSelectInput"
-                                        type="radio"
-                                        name="nowStayOverYn"
-                                        onChange={handleChangeNowStayOverYn}
-                                        value="y"
-                                        checked={
-                                            nowStayOverYn === 'y' ? true : false
-                                        }
-                                    />
-                                    <span className="selectInputText">
-                                        해당함
-                                    </span>
-                                    <input
-                                        className="paramSelectInput"
-                                        type="radio"
-                                        name="nowStayOverYn"
-                                        onChange={handleChangeNowStayOverYn}
-                                        value="n"
-                                        checked={
-                                            nowStayOverYn === 'n' ? true : false
-                                        }
-                                    />
-                                    <span className="selectInputText">
-                                        해당하지 않음
-                                    </span>
-                                </td>
-                            </tr>
-                        </table>
-
-                        <div className="buttonContainer">
-                            <span className="buttonPosition">
-                                <button
-                                    className="aptBackButton"
-                                    type="back"
-                                    onClick={() => {
-                                        history.goBack(-1);
-                                    }}
-                                >
-                                    이전
-                                </button>
-                            </span>
-                            <span className="buttonPosition">
-                                <button
-                                    className="aptNextButton"
-                                    type="button"
-                                    onClick={onClick}
-                                >
-                                    다음
-                                </button>
-                            </span>
-                        </div>
-                    </form>
+                <div className="buttonContainer">
+                    <span className="buttonPosition">
+                        <button
+                            className="aptBackButton"
+                            type="back"
+                            onClick={() => {
+                                history.goBack(-1);
+                            }}
+                        >
+                            이전
+                        </button>
+                    </span>
+                    <span className="buttonPosition">
+                        <button
+                            className="aptNextButton"
+                            type="button"
+                            onClick={onClick}
+                        >
+                            다음
+                        </button>
+                    </span>
                 </div>
             </div>
         </>
